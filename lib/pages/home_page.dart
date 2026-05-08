a1346a453222b4eb753a13fac544ec2ebee32721
vnch1 <katelyn.clarisia19@gmail.com>
2026-05-08 14:20:16 +0700

    buat homapage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEAF0F6),
+      body: SafeArea(
+        child: Column(
+          children: [
+
+            // HEADER
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 19, 67, 139),
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(20),
+                ),
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundImage:
+                        NetworkImage("https://i.pravatar.cc/150?img=3"),
+                  ),
+                  SizedBox(width: 12),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("welcome!",
+                          style: TextStyle(color: const Color.fromARGB(179, 255, 255, 255))),
+                      Text("Jane Doe",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            SizedBox(height: 16),
+
+            // SPECIALTIES
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("theme",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _chip("Wedding"),
+                _chip("Birthday"),
+                _chip("Gathering"),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            // EO LIST
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: EdgeInsets.only(bottom: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://i.pravatar.cc/150?img=${index + 1}"),
+                      ),
+                      title: Text("EO Name"),
+                      subtitle: Text("Jakarta"),
+                      trailing: Icon(Icons.arrow_forward_ios, size: 16),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _chip(String text) {
+    return Chip(
+      label: Text(text),
+      backgroundColor: Colors.blue[50],
+    );
+  }
+}
