893cbb74713e9d36e6140e9b58b474e215c44a26
Adel162-debug <adelliavalenciag@gmail.com>
2026-05-30 23:22:45 +0700

    perbaiki model

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package_page.dart';
+import 'package_screen.dart';
  
 // ─── Profile Page ─────────────────────────────────────────────────────────────
  
