583fcc40db6618f7386ca11bc91c7af9411df11d
vnch1 <katelyn.clarisia19@gmail.com>
2026-06-01 19:56:55 +0700

    solve conflict di main.dart, set global font: tinos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart'; // 1. Tetap import Google Fonts
+import 'package:google_fonts/google_fonts.dart';
 import 'package:eo_app/screens/signup_screen.dart';
 
 void main() {
@@ -7,7 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); // 2. Mengikuti format terbaru dari teman Anda
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +16,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF102B53), // Warna dasar dari teman Anda
+          seedColor: const Color(0xFF102B53),
         ),
         useMaterial3: true,
-        // 3. Set Tinos sebagai font global aplikasi
         fontFamily: GoogleFonts.tinos().fontFamily, 
       ),
       home: const SignupScreen(),
