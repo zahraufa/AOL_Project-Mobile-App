d01bede561464fbfc6753c8451a8dc99da9efa2a
vnch1 <katelyn.clarisia19@gmail.com>
2026-06-01 19:34:05 +0700

    fiturlogin: menyelesaikan screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
-import 'package:eo_app/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
-
-import 'screens/home_screen.dart';
-import 'screens/search_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:eo_app/screens/signup_screen.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'App Auth',
       debugShowCheckedModeBanner: false,
-      home:
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF1B2A47), 
+        primaryColor: const Color(0xFF1B2A47),
+        fontFamily: GoogleFonts.tinos().fontFamily,
+      ),
+      home: const SignupScreen(), 
     );
   }
 }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-class CustomTextField extends StatelessWidget{
+class CustomTextField extends StatefulWidget {
   final String label;
   final bool isObscure;
   final TextEditingController controller;
@@ -14,23 +13,35 @@ class CustomTextField extends StatelessWidget{
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late bool _isObscured;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.isObscure;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          label,
+          widget.label,
           style: const TextStyle(
-            color: Color(0xFF102b53),
+            color: Color(0xFF1B2A47),
             fontWeight: FontWeight.bold,
             fontSize: 14,
-            fontFamily: 'Tinos',
           ),
         ),
         const SizedBox(height: 8),
         Container(
           decoration: BoxDecoration(
-            color: const Color(0xFFABBCD6),
+            color: const Color(0xFFA8B8D0),
             borderRadius: BorderRadius.circular(10),
             boxShadow: [
               BoxShadow(
@@ -41,11 +52,25 @@ class CustomTextField extends StatelessWidget{
             ],
           ),
           child: TextField(
-            controller: controller,
-            obscureText: isObscure,
-            decoration: const InputDecoration(
+            controller: widget.controller,
+            obscureText: _isObscured,
+            decoration: InputDecoration(
               border: InputBorder.none,
-              contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+              contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+              
+              suffixIcon: widget.isObscure
+                  ? IconButton(
+                      icon: Icon(
+                        _isObscured ? Icons.visibility_off : Icons.visibility,
+                        color: const Color(0xFF1B2A47).withValues(alpha: 0.6),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isObscured = !_isObscured;
+                        });
+                      },
+                    )
+                  : null,
             ),
           ),
         ),
