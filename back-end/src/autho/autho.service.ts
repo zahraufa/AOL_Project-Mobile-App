e9c1742791e4ff561ee6a1c78b1877aa923ecdbe
zahraufa <zahratulauliafauzir@gmail.com>
2026-06-02 15:24:42 +0700

    add auth.service login and token

--- back-end/src/autho/autho.service.ts
+++ back-end/src/autho/autho.service.ts
@@ -1,15 +1,19 @@
-import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { AuthoSignupDto, AuthoLoginDto } from './dto/auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt/dist/jwt.service';
 
 @Injectable()
 export class AuthoService {
     private _prismaService: PrismaService;
 
-    constructor(prismaService: PrismaService) {
+    private _jwtService: JwtService;
+
+    constructor(prismaService: PrismaService, jwtService: JwtService) {
         this._prismaService = prismaService;
+        this._jwtService = jwtService;
     }
 
     async signUp(body: AuthoSignupDto) {
@@ -39,6 +43,38 @@ export class AuthoService {
                 Username: newUser.Username,
                 User_email: newUser.User_email,
                 User_PhoneNumber: newUser.User_PhoneNumber,
+            }
+        }
+    }
+
+    async login(body: AuthoLoginDto) {
+        const user = await this._prismaService.users.findUnique({
+            where: {
+                User_email: body.User_Email,
+            }
+        })
+
+        if(!user){
+            throw new BadRequestException('Invalid email or password');
+        }
+
+        const isPasswordValid = await bcrypt.compare(body.User_password, user.User_password);
+
+        if(!isPasswordValid){
+            throw new UnauthorizedException('Invalid email or password');
+        }
+
+        const payload = { User_ID: user.User_ID, User_email: user.User_email };
+        const token = this._jwtService.sign(payload);
+
+        return{
+            message: 'Login successful',
+            token: token,
+
+            user: {
+                User_ID: user.User_ID,
+                Username: user.Username,
+                User_email: user.User_email,
             }
         }
     }
