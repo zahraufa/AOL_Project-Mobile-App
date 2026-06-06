78e401d4006a65bffcbfb6bd15c518e528c0d3ae
zahraufa <zahratulauliafauzir@gmail.com>
2026-06-06 19:07:51 +0700

    Get All EO, by id, dan comparing

--- back-end/src/event_organizer/event_organizer.controller.ts
+++ back-end/src/event_organizer/event_organizer.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { EventOrganizerService } from './event_organizer.service';
 
 @Controller('event-organizer')
-export class EventOrganizerController {}
+export class EventOrganizerController {
+    private _eventOrganizerService: EventOrganizerService;
+
+    constructor(eventOrganizerService: EventOrganizerService) {
+        this._eventOrganizerService = eventOrganizerService;
+    }
+
+    @Get()
+    getALLEO(){
+        return this._eventOrganizerService.getAllEO();
+    }
+
+    @Get('comparing')
+    compareEOs(@Query('id') EO_IDs: string){
+        return this._eventOrganizerService.compareEOs(EO_IDs);
+    }
+
+    @Get(':id')
+    getEOByID(@Param('id') EO_ID: string){
+        return this._eventOrganizerService.getEOByID(Number(EO_ID));
+    }
+}
--- back-end/src/event_organizer/event_organizer.module.ts
+++ back-end/src/event_organizer/event_organizer.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EventOrganizerService } from './event_organizer.service';
 import { EventOrganizerController } from './event_organizer.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersModule } from 'src/users/users.module';
+import { AuthoModule } from 'src/autho/autho.module';
 
 @Module({
+  imports: [PrismaModule, AuthoModule, UsersModule],
   providers: [EventOrganizerService],
   controllers: [EventOrganizerController]
 })
