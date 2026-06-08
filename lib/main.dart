b14b66de3c32f3e4b1b48df5c7f9f9a11c01d500
vnch1 <katelyn.clarisia19@gmail.com>
2026-06-08 23:20:53 +0700

    ubah px layar

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF102B53)),
         useMaterial3: true,
-        fontFamily: GoogleFonts.alexandria().fontFamily, 
+        fontFamily: GoogleFonts.alexandria().fontFamily,
       ),
-      home: const WelcomeScreen()
+      
+      builder: (context, child) {
+        return Scaffold(
+          backgroundColor: Colors.grey[300],
+          body: Center(
+            child: Container(
+              width: 402,
+              height: 874,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 20,
+                    spreadRadius: 5,
+                  ),
+                ],
+              ),
+              child: child, 
+            ),
+          ),
+        );
+      },
+
+      home: const WelcomeScreen(),
     );
   }
 }
