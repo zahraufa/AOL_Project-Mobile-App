8cf15c3c8fb9519dd35352d37756f83a2ba51749
sanviaatj <sanviaatj@gmail.com>
2026-06-13 22:35:33 +0700

    comment

--- back-end/src/event_organizer/event_organizer.controller.ts
+++ back-end/src/event_organizer/event_organizer.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { EventOrganizerService } from './event_organizer.service';
 import { FindForEODto } from './dto/eo.dto';
 
+// ini untuk get all eo
 @Controller('event-organizer')
 export class EventOrganizerController {
     private _eventOrganizerService: EventOrganizerService;
